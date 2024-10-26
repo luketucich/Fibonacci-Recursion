2815aaeb38d828baec30fd83b9ce38ecd7a7b327
luketucich <ltucich@gmail.com>
2024-10-25 20:28:05 -0400

    Completed iterative solution for Fibonacci sequence

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("test");
+// Iterative Solution
+function fibs(n) {
+  const fib = [0, 1];
+  const res = [];
+
+  for (let i = 0; i < n; i++) {
+    if (res.length < 1) {
+      res.push(fib[0]);
+    } else if (res.length < 2) {
+      res.push(fib[1]);
+    } else {
+      res.push(res[i - 2] + res[i - 1]);
+    }
+  }
+
+  return res;
+}
+
+console.log(fibs(5));
+console.log(fibs(8));
